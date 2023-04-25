4151930b5b5d78901aecd67920a891f6fbba567c
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-04-25 12:59:02 +0300

    Initial commit

---
+++ src/main/java/com/Ronenii/Kaplat_server_excercise/KaplatServerExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.Ronenii.Kaplat_server_excercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaplatServerExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaplatServerExcerciseApplication.class, args);
+	}
+
+}
