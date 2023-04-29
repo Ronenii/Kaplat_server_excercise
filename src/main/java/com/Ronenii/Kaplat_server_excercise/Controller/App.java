7b440a1c94317155dfd0b3619c951959aa0e4124
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-04-29 13:02:59 +0300

    Minor additions including: 1) /health get mapping in "App" 2) Constructor in TODO

--- src/main/java/com/Ronenii/Kaplat_server_excercise/Controller/App.java
+++ src/main/java/com/Ronenii/Kaplat_server_excercise/Controller/App.java
@@ -1,4 +1,15 @@
 package com.Ronenii.Kaplat_server_excercise.Controller;
+import  com.Ronenii.Kaplat_server_excercise.Model.TODO;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class App {
+    public App(){
+    }
+
+    @GetMapping({"/todo/health"})
+    public String health()
+    {
+        return "OK";
+    }
 }
--- src/main/java/com/Ronenii/Kaplat_server_excercise/Model/TODO.java
+++ src/main/java/com/Ronenii/Kaplat_server_excercise/Model/TODO.java
@@ -1,21 +1,32 @@
 package com.Ronenii.Kaplat_server_excercise.Model;
 
-enum Status
+enum eStatus
 {
     PENDING,
     LATE,
     DONE
 }
 public class TODO {
+    static private int idCount = 1;
     private int id;
     private String Title, Content;
-    private long DueDate;
-    private Status Status;
+    private long dueDate;
+    private eStatus Status;
+
+    public TODO(String Title, String Content, long dueDate, String Status)
+    {
+        this.id = idCount++;
+        this.Title = Title;
+        this.Content = Content;
+        this.dueDate = dueDate;
+        this.Status = eStatus.valueOf(Status);
+        System.out.print(this);
+    }
 
     public String toString(){
-        return "TODO(id=" + this.id +
+        return "TODO(id=" + id +
                 ", Title=" + this.Title +
                 ", Content=" + this.Content +
-                ", DueDate=" + this.DueDate +
+                ", DueDate=" + this.dueDate +
                 ", Status=" + this.Status + ")";}
 }
